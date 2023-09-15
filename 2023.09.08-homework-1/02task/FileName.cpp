577ade9e286b2fc19de2a370fa95f2c577efdb73
Pavel <st117168@student.spbu.ru>
2023-09-15 10:44:04 +0300

    Lesson 1 code edit_1

--- 2023.09.08-homework-1/02task/FileName.cpp
+++ 2023.09.08-homework-1/02task/FileName.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	int N = 0;
+	int n = 0;
 	int a = 0;
 	int b = 0;
-	std::cin >> N;
-	a = abs(N - 1) / (N - 1);	// знак числа N
-	b = 1 + a * (N - 1);	// количество чисел
-	std::cout << (1 + N) * b / 2; // сумма чисел
+	std::cin >> n;
+	a = abs(n - 1) / (n - 1);	// знак числа N
+	b = 1 + a * (n - 1);	// количество чисел
+	std::cout << (1 + n) * b / 2; // сумма чисел
 	return 0;
 }
--- 2023.09.08-homework-1/05task/FileName.cpp
+++ 2023.09.08-homework-1/05task/FileName.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	int U = 0;
-	int T = 0;
+	int u = 0;
+	int t = 0;
 	int a = 0;
-	int S = 0;
-	std::cin >> U;
-	std::cin >> T;
-	a = U * T % 109;
-	S = (a + 109) % 109 + 1;
-	std::cout << S;
+	int s = 0;
+	std::cin >> u;
+	std::cin >> t;
+	a = u * t % 109;
+	s = (a + 109) % 109 + 1;
+	std::cout << s;
 	return 0;
 }
